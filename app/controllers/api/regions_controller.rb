d91b6f03ec4caa49887777116637bcf92824638f
Jacob Shorty <jakeshorty@gmail.com>
2015-05-30 19:26:54 -0400

    Small refactor to regions_controller

--- app/controllers/api/regions_controller.rb
+++ app/controllers/api/regions_controller.rb
@@ -4,10 +4,13 @@ module Api
 
     def create
       @region = Region.find_most_specific(region_params)
+
       if !@region
-        render json: ["Sorry, we couldn't find this region. Please confirm your spelling, or be less specific."], status: 404
+        render json: ["Sorry, we couldn't find this region."], status: 404
+
       elsif @region.users.include?(current_user)
         render json: ["You are already studying that region!"], status: 422
+
       else
         @user_region = UserRegion.new(region_id: @region.id,
                                       user_id: current_user.id,
@@ -48,10 +51,12 @@ module Api
     private
 
       def region_params
-        params[:region][:county] = nil if params[:region][:county] == ""
-        params[:region][:state] = nil if params[:region][:state] == ""
-        params[:region][:country] = nil if params[:region][:country] == ""
+        [:country, :state, :country].each do |scope|
+          params[:region][scope] = nil if params[:region][scope].blank?
+        end
         params.require(:region).permit(:id, :county, :state, :country)
       end
+
+      def
   end
 end
